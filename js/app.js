f87cca1f9120a8602883b1ee6978ea38d57b46ee
Ryne Emory <ryneemory@gmail.com>
2016-06-27 20:49:36 -0600

    finished basic routes

--- js/app.js
+++ js/app.js
@@ -6,8 +6,20 @@ app.config(function($routeProvider) {
         templateUrl: 'partials/about.html',
         controller: 'AboutController'
       })
+      .when('/education', {
+        templateUrl: 'partials/education.html',
+        controller: 'EducationController'
+      })
+      .when('/projects', {
+        templateUrl: 'partials/projects.html',
+        controller: 'ProjectsController'
+      })
       .when('/experience', {
         templateUrl: 'partials/experience.html',
         controller: 'ExperienceController'
+      })
+      .when('/skills', {
+        templateUrl: 'partials/skills.html',
+        controller: 'SkillsController'
       })
 });
